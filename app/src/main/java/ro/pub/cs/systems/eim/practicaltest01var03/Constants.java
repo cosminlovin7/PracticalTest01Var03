684e2e7891904ba3b77074ead0871ee0763b044e
Cosmin Lovin <cosmiinreus@gmail.com>
2023-04-06 08:40:42 +0300

    finished B.1

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var03/Constants.java
@@ -0,0 +1,7 @@
+package ro.pub.cs.systems.eim.practicaltest01var03;
+
+public class Constants {
+    public static final String TAG = "cosminel_debugheaza";
+    public static final String ONLY_NUMBERS = "Please introduce correct numbers";
+    public static final String FIRST_NUMBER_TEXTVIEW = "FIRST_NUMBER_TEXTVIEW";
+}
--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var03/PracticalTest01Var03MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var03/PracticalTest01Var03MainActivity.java
@@ -3,12 +3,87 @@ package ro.pub.cs.systems.eim.practicaltest01var03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class PracticalTest01Var03MainActivity extends AppCompatActivity {
+
+    private ButtonClickListener buttonClickListener = new ButtonClickListener();
+    private EditText firstNumberEditText;
+    private EditText secondNumberEditText;
+    private EditText resultEditText;
+    private Button buttonAdd;
+    private Button buttonMinus;
+
+    private boolean isCorrectString(final String number) {
+        if (number.length() == 0)
+            return false;
+
+        for (int i = 0; i < number.length(); i++) {
+            if (number.charAt(i) >= '0' && number.charAt(i) <= '9')
+                continue;
+
+            return false;
+        }
+
+        return true;
+    }
+
+    private class ButtonClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            Integer buttonId = view.getId();
+            String firstNumber = null;
+            String secondNumber = null;
+            switch(buttonId) {
+                case R.id.button_add:
+                    firstNumber = firstNumberEditText.getText().toString();
+                    secondNumber = secondNumberEditText.getText().toString();
+
+                    if (firstNumber.length() != 0 && secondNumber.length() != 0) {
+                        Double firstIntNumber = Double.parseDouble(firstNumber);
+                        Double secondIntNumber = Double.parseDouble(secondNumber);
+                        double result = firstIntNumber + secondIntNumber;
+
+                        resultEditText.setText(firstNumber +" + " + secondNumber + " = " + result);
+                    } else {
+                        Toast.makeText(PracticalTest01Var03MainActivity.this, Constants.ONLY_NUMBERS, Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+                case R.id.button_minus:
+                    firstNumber = firstNumberEditText.getText().toString();
+                    secondNumber = secondNumberEditText.getText().toString();
+
+                    if (firstNumber.length() != 0 && secondNumber.length() != 0) {
+                        Double firstIntNumber = Double.parseDouble(firstNumber);
+                        Double secondIntNumber = Double.parseDouble(secondNumber);
+                        double result = firstIntNumber - secondIntNumber;
+
+                        resultEditText.setText(firstNumber +" - " + secondNumber + " = " + result);
+                    } else {
+                        Toast.makeText(PracticalTest01Var03MainActivity.this, Constants.ONLY_NUMBERS, Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_var03_main);
+
+        firstNumberEditText = findViewById(R.id.first_number_textview);
+        secondNumberEditText = findViewById(R.id.second_number_textview);
+        resultEditText = findViewById(R.id.result_textview);
+        buttonAdd = findViewById(R.id.button_add);
+        buttonMinus = findViewById(R.id.button_minus);
+
+        buttonAdd.setOnClickListener(buttonClickListener);
+        buttonMinus.setOnClickListener(buttonClickListener);
     }
 }
